593ab766c1216b60e9e73fc74f67516e0c763dce
Gule Sheikh <gulesh@gmail.com>
2014-01-09 15:35:19 -0600

    Commiting mySafari with Andrew and Gule Completed

--- mySafari/ViewController.m
+++ mySafari/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIScrollViewDelegate>
+
+{
+
+    __weak IBOutlet UIWebView *myWebView;
+    __weak IBOutlet UITextField *myURLTextField;
+
+    
+}
 
 @end
 
@@ -18,7 +26,79 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    myWebView.scrollView.delegate = self;
+    
 }
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSURL *myURL = [NSURL URLWithString:myURLTextField.text];
+    
+    if ([myURLTextField.text hasPrefix:@"http://"]){
+   
+    }
+    else{
+        NSString *http = [NSString stringWithFormat:@"http://%@", myURLTextField.text];
+        
+         myURL = [NSURL URLWithString:http];
+    }
+        
+    NSURLRequest *rq = [NSURLRequest requestWithURL:myURL];
+    [myWebView loadRequest:rq];
+    
+    
+    [myURLTextField resignFirstResponder];
+    
+    return YES;
+}
+- (IBAction)onBackButtonPressed:(id)sender
+{
+    [myWebView goBack];
+    
+}
+- (IBAction)onReloadButtonPressed:(id)sender
+{
+    [myWebView reload];
+}
+
+
+- (IBAction)onStopLoadingButtonPressed:(id)sender
+{
+    [myWebView stopLoading];
+    
+}
+
+
+- (IBAction)onForwardButtonPressed:(id)sender
+{
+    [myWebView goForward];
+    
+}
+- (IBAction)onTeaserButtonPressed:(id)sender
+{
+    UIAlertView *myAlert = [[UIAlertView alloc]initWithTitle:@"Coming Soon" message:@"Sorry for the Inconvenience" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    [myAlert show];
+    
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGPoint translation = [scrollView.panGestureRecognizer translationInView:scrollView.superview];
+    
+    if(translation.y > 0)
+    {
+        myURLTextField.alpha += .05;
+        // react to dragging up
+    } else
+    {
+        myURLTextField.alpha -= .05;
+        // react to dragging down
+    }
+}
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
