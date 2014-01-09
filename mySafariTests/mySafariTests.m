16f6e540ba8cc22f8ce666e1f0c9ce3e3f026dec
Gule Sheikh <gulesh@gmail.com>
2014-01-09 13:37:52 -0600

    Initial Commit

---
+++ mySafariTests/mySafariTests.m
@@ -0,0 +1,34 @@
+//
+//  mySafariTests.m
+//  mySafariTests
+//
+//  Created by gule on 1/9/2014.
+//  Copyright (c) 2014 gule. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mySafariTests : XCTestCase
+
+@end
+
+@implementation mySafariTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
